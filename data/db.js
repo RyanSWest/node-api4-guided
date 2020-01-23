c4f0027723ae2512617264c3c4a5ad8369486d41
RyanSWest <west56309@gmail.com>
2020-01-22 21:36:10 -0600

    new commit

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const environment = process.env.NODE_ENV || 'development';
+// const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
-
+const environment = 'development';
 module.exports = knex(config);
